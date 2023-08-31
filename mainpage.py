7b5e02271312f75ac4fb7c414f18f34b3c75f540
tamefelis <126751258+tamefelis@users.noreply.github.com>
2023-09-01 00:01:02 +0800

    Add files via upload

---
+++ mainpage.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+PAGES = {
+    st.title('Welcome to the miscellaneous tool of ABLE')
+    #"Date Screener": DateScreener.date_screener_page,
+    # Add more pages here as your project expands
+}
+
+#def main():
+#   st.sidebar.title('Navigation')
+#   page_selection = st.sidebar.radio('Go to', list(PAGES.keys()))
+#
+#   # Call the appropriate page function based on selection
+#   page = PAGES[page_selection]
+#   page()
+#
+#if __name__ == "__main__":
+#    main()
